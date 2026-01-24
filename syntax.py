8216bd8c86d53066cf642c0c2a017c5a4e3ef6fc
medelbekmika <mikamedelbek01@gmail.com>
2026-01-25 02:23:33 +0500

    second topic

---
+++ syntax.py
@@ -0,0 +1,28 @@
+#1
+if 5 > 2:
+  print("Five is greater than two")
+
+#2
+age1=7
+age2=9
+
+if age1 < age2 :
+  print(age2,"is greater than",age1)
+
+
+#3
+num1=11
+num2=55
+
+if num1 > num2 :
+  print(num1,"is greater than",num2)
+else:
+  print(num2,"is greater than",num1)
+
+#4
+if 0==0 :
+  print("elements are equal")
+
+#5
+if 1 > 0 :
+  print("1 is greater than 0")
