e34211af232b697624c41a1d0de8d74cb04ebc42
medelbekmika <mikamedelbek01@gmail.com>
2026-01-26 15:27:51 +0500

    comments in python

---
+++ comments.py
@@ -0,0 +1,21 @@
+#this is a comment
+x = 5 
+print(x)
+
+#comment
+print(5 + 7)
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+'''
+this is a next version 
+of comment which writes 
+in more than one line 
+'''
+
+#basic comment for the one line
+print("HELLO")
