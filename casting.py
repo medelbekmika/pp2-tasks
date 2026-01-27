79526fe9b41eb4c955aa5340f40a41bc8599d6d8
medelbekmika <mikamedelbek01@gmail.com>
2026-01-27 15:15:33 +0500

    Implement type casting examples in casting.py
    Add examples of type casting in Python.

---
+++ casting.py
@@ -0,0 +1,24 @@
+#1
+a = "5"
+b = int(a)
+print(b)
+
+#2
+x = 1000
+y = str(x)
+print(y)
+
+#3
+n = 4.878787
+m = int(n)
+print(m)
+
+#4
+value = 0
+result = bool(value)
+print(result)
+
+#5
+num = 3
+num2 = float(num)
+print(num2)
