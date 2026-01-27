abe4d7b1face1e7274067fae854a8fe9579fa359
medelbekmika <mikamedelbek01@gmail.com>
2026-01-27 15:10:01 +0500

    Add datatype examples in datatype.py

---
+++ datatype.py
@@ -0,0 +1,25 @@
+#1
+x=str("Hello World")
+print(type(x))
+print(x)
+
+#2
+x = list(("apple", "watermelon", "cherry"))
+print(x)
+print(type(x)) 
+
+#3
+x = bool(8)
+print(x)
+print(type(x)) 
+
+#4
+x=float(1.11)
+print(x)
+print(type(x))
+
+#5
+x=int(9)
+print(x)
+print(type(x))
+
