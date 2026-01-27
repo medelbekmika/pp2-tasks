39685063630bd47cda0edf04872d28e3187724d2
medelbekmika <mikamedelbek01@gmail.com>
2026-01-27 15:18:28 +0500

    Add string manipulation examples in strings.py

---
+++ strings.py
@@ -0,0 +1,21 @@
+#1
+a = "Hello, World!"
+print(a[1:5])
+
+#2
+a = "Hello, World!"
+print(a[:3])
+
+#3
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#4
+a = "Hello, World!"
+print(a.lower())
+
+#5
+a = "Hello, World!"
+print(a.upper())
