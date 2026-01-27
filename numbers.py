ad4fff3da54c17921d6a1a0bed10b11017958dc4
medelbekmika <mikamedelbek01@gmail.com>
2026-01-27 15:13:21 +0500

    Add numeric type examples in numbers.py
    Add examples demonstrating different numeric types in Python.

---
+++ numbers.py
@@ -0,0 +1,40 @@
+#1
+x = 1
+y = 3565
+z = -325.77
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#2
+x = 1.10
+y = 5555.555
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#3
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#4
+x = 3+7j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#5
+import random
+print(random.randrange(1, 10))
+
