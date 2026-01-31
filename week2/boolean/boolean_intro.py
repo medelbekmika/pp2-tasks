26e89a74224bf93ed2535d277fb966fea24f1135
medelbekmika <mikamedelbek01@gmail.com>
2026-01-31 20:39:50 +0500

    Update boolean_intro.py

--- week2/boolean/boolean_intro.py
+++ week2/boolean/boolean_intro.py
@@ -1,1 +1,24 @@
+#1
+print(bool("Hello"))
+print(bool(15))
+#2
+x = "Hello"
+y = 15
 
+print(bool(x))
+print(bool(y))
+#3
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+#4
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+bool(True)
+#5
+print(bool(1))
