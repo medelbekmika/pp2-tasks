8f3d382dc3ead3529e0fb5051d0ddec26d9bc00a
medelbekmika <mikamedelbek01@gmail.com>
2026-01-31 21:05:29 +0500

    Add multiple if statements for number checks

--- week2/if_else/if_statement.py
+++ week2/if_else/if_statement.py
@@ -1,1 +1,23 @@
-
+#1
+number = 15
+if number > 0:
+  print("The number is positive")
+#2
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+#3
+a=int(input())
+if a == 10 :
+  print("good")
+#4
+a=int(input())
+if a == 0:
+  print(a,"is equal to 0")
+#5
+num=9
+if 9 >=9 :
+  print("YES")
+  
