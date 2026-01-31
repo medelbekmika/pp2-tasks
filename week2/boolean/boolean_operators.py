c3c008b74c252f6c49492016a2d9cc131e240173
medelbekmika <mikamedelbek01@gmail.com>
2026-01-31 20:49:55 +0500

    Create boolean_operators.py with boolean examples
    Add boolean operators examples in boolean_operators.py

---
+++ week2/boolean/boolean_operators.py
@@ -0,0 +1,17 @@
+#1
+print(True and True)
+print(True and False)
+#2
+print(True or True)
+print(True or False)
+#3
+print(not True)
+print(not False)
+#4
+a = True
+b = False
+c = True
+result = a and b or c
+print(result)  
+#5
+print(False and False)
