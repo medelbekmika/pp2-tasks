8fbfa8192efdae8997ca07bdb71a98bebaff5be8
medelbekmika <mikamedelbek01@gmail.com>
2026-01-31 20:47:19 +0500

    Create boolean_comparison.py

---
+++ week2/boolean/boolean_comparison.py
@@ -0,0 +1,20 @@
+#1
+a = 10
+b = 5
+print(a > b)   
+print(a < b)  
+#2
+print(10>8)
+print(10==8)
+print(10!=8)
+#3
+a=int(input())
+b=int(input())
+print(a>b)
+#4
+fruit="apple"
+print(fruit == "apple")
+#5
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
