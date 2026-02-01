21a263350dd0e1402bd0024ce14ede4bb593d591
medelbekmika <mikamedelbek01@gmail.com>
2026-02-01 17:35:21 +0500

    Add if-elif-else examples for grading and comparisons

---
+++ week2/if_else/if_elif_else.py
@@ -0,0 +1,51 @@
+#1
+score = 85
+
+if score >= 90:
+    print("Grade A")
+elif score >= 75:
+    print("Grade B")
+elif score >= 60:
+    print("Grade C")
+else:
+    print("Fail")
+#2
+a = ine(input())
+
+if a % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+#3
+num = int(input())
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+#4
+temp = int(input())
+
+if temp > 35:
+    print("It's very hot")
+elif temp > 25:
+    print("Nice weather")
+elif temp > 15:
+    print("A bit cool")
+else:
+    print("Cold weather")
+
+#5
+a = int(input())
+b = int(input())
+
+if a > b :
+  print("a is greater than b")
+elif a < b :
+  print("b is greater than a")
+else:
+  print("a is equal to b")
+  
