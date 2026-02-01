d6ffb4fe0d8cee889110c41b8ef2648614b85351
medelbekmika <mikamedelbek01@gmail.com>
2026-02-01 18:25:43 +0500

    Implement examples of 'continue' in for loops
    Add multiple examples of using 'continue' in loops.

---
+++ week2/loops/for_continue.py
@@ -0,0 +1,26 @@
+#1
+for i in range(1, 6):
+    if i == 3:
+        continue  
+    print(i)
+#2
+for i in range(1, 6):
+    if i % 2 == 0:
+        continue
+    print(i)
+#3
+for i in range(1, 5):
+    if i == 2:
+        continue  
+    print(i)
+
+#4
+for i in range(1, 6):
+    if i % 2 == 0:
+        continue 
+    print(i)
+#5
+for i in range(1, 7):
+    if i == 5:
+        continue 
+    print(i)
