c3ab679a778a2ce59feda6fe6b23d684029f55e1
medelbekmika <mikamedelbek01@gmail.com>
2026-02-01 18:20:51 +0500

    Add for_loop.py with various loop examples

---
+++ week2/loops/for_loop.py
@@ -0,0 +1,23 @@
+#1
+for i in range(1, 6):
+    print(i)
+#2
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+#3
+total = 0
+for i in range(1, 6):
+    total += i
+print("sum =", total)
+#4
+for i in range(1, 10):
+    if i == 4:
+        break
+    print(i)
+#5
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
+
