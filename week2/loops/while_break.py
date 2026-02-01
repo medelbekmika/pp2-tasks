d5f9a0dde3f9400d03998e01f0689c92d68e2494
medelbekmika <mikamedelbek01@gmail.com>
2026-02-01 18:16:10 +0500

    Add examples of while loops with break statements
    Implement multiple while loops demonstrating break functionality.

---
+++ week2/loops/while_break.py
@@ -0,0 +1,34 @@
+#1
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+#2
+i = 1
+while True:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+#3
+i = int(input())
+while i <= 5:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+#4
+i = 1
+while i <= 10:
+    if i == 5:
+        break
+    print(i)
+    i += 1
+#5
+while True:
+    n = input()
+    if n == "0":
+        break
+    print(n)
