b7b0d2287cdb3d77c0b3c6289d8b155d284a1d8e
medelbekmika <mikamedelbek01@gmail.com>
2026-02-01 17:46:39 +0500

    Create while_loop.py

---
+++ week2/loops/while_loop.py
@@ -0,0 +1,27 @@
+#1
+a=int(input())
+while(a < 5):
+  print(a)
+  a+=1
+#2
+a=int(input())
+while(a == 7):
+  print(a)
+  a+=1
+#3
+count = 5
+while count > 0:
+    print(count)
+    count -= 1
+#4
+i = 1
+while i <= 3:
+    print(i)
+    i += 1
+else:
+    print("Loop finished")
+#5
+password = ""
+while password != "python":
+    password = input("Enter password: ")
+print("Access granted ")
