e063b5306b7ae226ad6a336cc4b36f007effcaaa
medelbekmika <mikamedelbek01@gmail.com>
2026-02-10 14:12:31 +0500

    Implement temperature conversion functions
    Added functions to convert Fahrenheit to Celsius and print results.

---
+++ week3/functions/basic_functions.py
@@ -0,0 +1,28 @@
+#1
+def my_function():
+  print("Hello from a function")
+
+#2
+def my_function():
+  print("Hello from a function")
+
+my_function()
+#3
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+#4
+temp1 = 77
+celsius1 = (temp1 - 32) * 5 / 9
+print(celsius1)
+
+#5
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
