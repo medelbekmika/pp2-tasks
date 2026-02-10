8685bd00c4689f1c9a364441cad083c4c8357705
medelbekmika <mikamedelbek01@gmail.com>
2026-02-10 14:18:22 +0500

    Implement sum function examples in return_values.py
    Added multiple examples of a sum function with different input methods.

---
+++ week3/functions/return_values.py
@@ -0,0 +1,36 @@
+#1
+a=1
+b=22
+def sum(x , y):
+  return x + y
+
+z=sum(a , b)
+print(z)
+
+#2
+a=1
+b=22
+def sum(x , y):
+  return x + y
+print(sum(a , b)
+
+#3
+a=int(input())
+b=int(input())
+def sum(x , y):
+  return x + y
+print(sum(a , b)
+
+#4
+a=int(input())
+b=22
+def sum(x , y):
+  return x + y
+print(sum(a , b)
+
+#5
+a=1
+b=int(input())
+def sum(x , y):
+  return x + y
+print(sum(a , b)
