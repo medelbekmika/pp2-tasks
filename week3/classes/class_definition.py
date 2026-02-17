6a3d26eb4835cf736370e6403e43cf311d4dd90e
medelbekmika <mikamedelbek01@gmail.com>
2026-02-17 22:28:45 +0500

    Add initial class definitions for Person, Car, Dog, Book, and Student

---
+++ week3/classes/class_definition.py
@@ -0,0 +1,19 @@
+# 1
+class Person:
+    pass
+
+# 2
+class Car:
+    brand = "Toyota"
+
+# 3
+class Dog:
+    species = "Canine"
+
+# 4
+class Book:
+    title = "Unknown"
+
+# 5
+class Student:
+    grade = 0
