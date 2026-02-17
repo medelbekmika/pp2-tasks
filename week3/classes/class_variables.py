b66b1a2f2b6757a8d1e200196bdca5325c479a68
medelbekmika <mikamedelbek01@gmail.com>
2026-02-17 22:29:51 +0500

    Add class definitions with class variables

---
+++ week3/classes/class_variables.py
@@ -0,0 +1,19 @@
+# 1
+class Person:
+    species = "Human"
+
+# 2
+class Car:
+    wheels = 4
+
+# 3
+class Dog:
+    kingdom = "Animal"
+
+# 4
+class Student:
+    school = "High School"
+
+# 5
+class Employee:
+    company = "TechCorp"
