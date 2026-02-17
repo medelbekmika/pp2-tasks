77922fd4c4e5e3b474da040b4d92eed7d008bc30
medelbekmika <mikamedelbek01@gmail.com>
2026-02-17 20:45:51 +0500

    Add lambda functions for various operations

---
+++ week3/lambda/lambda_basics.py
@@ -0,0 +1,19 @@
+ # 1
+square = lambda x: x ** 2
+print(square(4))
+
+# 2
+add = lambda a, b: a + b
+print(add(3, 7))
+
+# 3
+is_even = lambda x: x % 2 == 0
+print(is_even(10))
+
+# 4
+greet = lambda name: f"Hello, {name}!"
+print(greet("Mika"))
+
+# 5
+max_of_two = lambda a, b: a if a > b else b
+print(max_of_two(5, 9))
