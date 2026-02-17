36922d98bef496ce0de885d116bb15dd7b58a2be
medelbekmika <mikamedelbek01@gmail.com>
2026-02-17 22:29:04 +0500

    Add classes for Person, Car, Dog, Book, and Student

---
+++ week3/classes/init_method.py
@@ -0,0 +1,28 @@
+# 1
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+# 2
+class Car:
+    def __init__(self, brand, year):
+        self.brand = brand
+        self.year = year
+
+# 3
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# 4
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+# 5
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
