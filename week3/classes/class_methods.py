4b87a729eea6a167deedcd1cd07d604ad3458570
medelbekmika <mikamedelbek01@gmail.com>
2026-02-17 22:29:24 +0500

    Add classes for Person, Car, Dog, Calculator, and Student

---
+++ week3/classes/class_methods.py
@@ -0,0 +1,24 @@
+# 1
+class Person:
+    def greet(self):
+        print("Hello!")
+
+# 2
+class Car:
+    def start(self):
+        print("Car started")
+
+# 3
+class Dog:
+    def bark(self):
+        print("Woof!")
+
+# 4
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+# 5
+class Student:
+    def info(self):
+        return f"Name: {self.name}"
