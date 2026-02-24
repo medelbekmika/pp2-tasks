898f5f43b697ecaa56b387ccd1c0bd1747e01b10
medelbekmika <mikamedelbek01@gmail.com>
2026-02-24 21:00:38 +0500

    Add math.py for area calculations and angle conversion
    Implement various area calculations and angle conversion.

---
+++ week4/math.py
@@ -0,0 +1,25 @@
+#1
+import math
+n=int(input())
+rad=math.radians(n)
+print(rad)
+
+#2
+a=int(input())
+b=int(input())
+c=int(input())
+area=((b+c)/2 ) * a
+print(area)
+
+#3
+import math
+n=int(input())
+s=int(input())
+area = n*s*s/4*math.tan(math.pi / n)
+print(area)
+
+#4
+a=int(input())
+b=int(input())
+area=a*b
+print(area)
