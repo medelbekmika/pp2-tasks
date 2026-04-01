91a33d75da1784ac18e409db0c901350da5ecaaf
medelbekmika <mikamedelbek01@gmail.com>
2026-04-01 10:20:10 +0500

    Implement database connection function
    Add a database connection function using psycopg2

---
+++ 7practice/connect.py
@@ -0,0 +1,11 @@
+
+
+import psycopg2
+from config import DB_CONFIG
+def connect():
+    try:
+        conn = psycopg2.connect(**DB_CONFIG)
+        return conn
+    except Exception as e:
+        print("Error connecting to database:", e)
+        return None
