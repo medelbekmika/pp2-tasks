28dc730f4d3db6dbf55af4ff0f5e5777045a2209
medelbekmika <mikamedelbek01@gmail.com>
2026-04-10 17:51:52 +0500

    Add database connection function using psycopg2

--- 8practice/connect.py
+++ 8practice/connect.py
@@ -1,1 +1,11 @@
+import psycopg2
+from config import host, user, password, database
 
+def connect_db():
+    conn = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    return conn
